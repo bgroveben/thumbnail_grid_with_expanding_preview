a0c866b1cbfe4a502ce6113036ad4acac4bb908e
bgrove1974 <bgroveben@gmail.com>
2016-02-15 00:17:37 -0800

    Cache some elements, initialize some variables

--- js/grid.js
+++ js/grid.js
@@ -1,1 +1,32 @@
-
+// list of items
+var $grid = $( '#og-grid' ),
+  // the items
+  $items = $grid.children( 'li' ),
+  // current expanded item's index
+  current = -1,
+  // position (top) of the xpanded item
+  // used to know if the preview will expand in a different row
+  previewPos = -1,
+  // extra amount of pixels to scroll the window
+  scrollExtra = 0,
+  // extra margin when expanded (between the preview element and the next item row)
+  marginExpanded = 10,
+  $window = $( window ), winsize,
+  $body = $( 'html, body' ),
+  // transitioned events
+  transEndEventNames = {
+    'WebkitTransition' : 'webkitTransitionEnd',
+    'MozTransition' : 'transitioned',
+    'OTransition' : 'oTransitionEnd',
+    'msTransition' : 'msTransitionEnd',
+    'transition' : 'transitioned'
+  },
+  transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+  // support for csstransitions
+  support = Modernizr.csstransitions,
+  // default settings
+  settings = {
+    minHeight : 500,
+    speed : 350,
+    easing : 'ease'
+  };
